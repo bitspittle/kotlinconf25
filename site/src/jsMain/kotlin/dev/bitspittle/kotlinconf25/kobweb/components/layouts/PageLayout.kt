d7cd6a60ff1daf8243c156977e85c3db9428e858
David Herman <bitspittle@gmail.com>
2025-05-02 13:20:00 -0700

    Add purple -> black gradient in dark mode
    This roughly matches a pattern that the official Kotlin
    slides are using

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/layouts/PageLayout.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/layouts/PageLayout.kt
@@ -2,14 +2,31 @@ package dev.bitspittle.kotlinconf25.kobweb.components.layouts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import com.varabyte.kobweb.compose.css.functions.LinearGradient
+import com.varabyte.kobweb.compose.css.functions.linearGradient
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundImage
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.minHeight
+import com.varabyte.kobweb.compose.ui.thenIf
 import com.varabyte.kobweb.core.PageContext
 import com.varabyte.kobweb.core.layout.Layout
+import com.varabyte.kobweb.silk.style.CssStyle
+import com.varabyte.kobweb.silk.style.base
+import com.varabyte.kobweb.silk.style.toModifier
 import org.jetbrains.compose.web.css.vh
+
+val PageLayoutStyle = CssStyle.base {
+    val veryDarkPurple = Colors.Purple.darkened(0.7f)
+    Modifier
+        .fillMaxWidth().minHeight(100.vh)
+        .thenIf(colorMode.isDark,
+            Modifier.backgroundImage(linearGradient(veryDarkPurple, Colors.Black, LinearGradient.Direction.ToBottomRight))
+        )
+}
 
 @Layout
 @Composable
@@ -19,7 +36,7 @@ fun PageLayout(ctx: PageContext, content: @Composable () -> Unit) {
         js("Prism.highlightAll()")
     }
 
-    Box(Modifier.fillMaxWidth().minHeight(100.vh), contentAlignment = Alignment.Center) {
+    Box(PageLayoutStyle.toModifier(), contentAlignment = Alignment.Center) {
         content()
     }
 }
