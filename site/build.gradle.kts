6d83aaf7f028761de697b095632276543f54b690
David Herman <bitspittle@gmail.com>
2025-05-02 10:47:52 -0700

    Add fonts that we'll use in the talk

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+import kotlinx.html.link
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
@@ -14,6 +15,22 @@ kobweb {
     app {
         index {
             description.set("KotlinConf 2025 - Build Websites in Kotlin & Compose HTML with Kobweb")
+
+            head.add {
+                link {
+                    rel = "preconnect"
+                    href = "https://fonts.googleapis.com"
+                }
+                link {
+                    rel = "preconnect"
+                    href = "https://fonts.gstatic.com"
+                    attributes["crossorigin"] = ""
+                }
+                link {
+                    rel = "stylesheet"
+                    href = "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+                }
+            }
         }
     }
 }
--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/AppEntry.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/AppEntry.kt
@@ -1,11 +1,33 @@
 package dev.bitspittle.kotlinconf25.kobweb
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.OverflowWrap
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.overflowWrap
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.init.registerStyleBase
 import com.varabyte.kobweb.silk.style.common.SmoothColorStyle
 import com.varabyte.kobweb.silk.style.toModifier
+
+@InitSilk
+fun initSilk(ctx: InitSilkContext) {
+    ctx.stylesheet.apply {
+        registerStyleBase("body") {
+            Modifier
+                .fontFamily("Inter", "sans-serif")
+                .overflowWrap(OverflowWrap.BreakWord)
+        }
+
+        registerStyleBase("code, pre") {
+            Modifier.fontFamily("Roboto Mono", "monospace")
+        }
+    }
+}
 
 @App
 @Composable
