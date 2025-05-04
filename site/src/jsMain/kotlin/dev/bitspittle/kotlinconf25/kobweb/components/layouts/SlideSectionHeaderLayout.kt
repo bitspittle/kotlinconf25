b29857d6f5a972d85bd3de5cedb66d55faa097c7
David Herman <bitspittle@gmail.com>
2025-05-03 20:57:10 -0700

    Nit: Refactor section header layout, taking ownership of H1

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/layouts/SlideSectionHeaderLayout.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/layouts/SlideSectionHeaderLayout.kt
@@ -2,20 +2,28 @@ package dev.bitspittle.kotlinconf25.kobweb.components.layouts
 
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.css.Width
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.layout.Layout
 import org.jetbrains.compose.web.css.percent
-
+import org.jetbrains.compose.web.dom.H1
 
 @Layout(".components.layouts.SlideLayout")
 @Composable
 fun SlideSectionHeaderLayout(content: @Composable () -> Unit) {
-    Box(Modifier.fillMaxSize().textAlign(TextAlign.Center).padding(20.percent), contentAlignment = Alignment.TopCenter) {
-        content()
+    Box(
+        Modifier.fillMaxSize().textAlign(TextAlign.Center).padding(20.percent),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        H1(Modifier.width(Width.MaxContent).toAttrs()) {
+            content()
+        }
     }
 }
--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/cmp/HtmlVsWasm1.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/cmp/HtmlVsWasm1.kt
@@ -1,27 +1,21 @@
 package dev.bitspittle.kotlinconf25.kobweb.pages.cmp
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.Width
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.color
-import com.varabyte.kobweb.compose.ui.modifiers.width
-import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.layout.Layout
 import com.varabyte.kobweb.silk.components.text.SpanText
 import org.jetbrains.compose.web.dom.Br
-import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Text
 
 @Page
 @Composable
 @Layout(".components.layouts.SlideSectionHeaderLayout")
 fun HtmlVsWasm1Page() {
-    H1(Modifier.width(Width.MaxContent).toAttrs()) {
-        Text("Compose HTML")
-        SpanText(" vs", Modifier.color(Colors.Red))
-        Br()
-        Text("CMP For Web (Wasm)")
-    }
+    Text("Compose HTML")
+    SpanText(" vs", Modifier.color(Colors.Red))
+    Br()
+    Text("CMP For Web (Wasm)")
 }
