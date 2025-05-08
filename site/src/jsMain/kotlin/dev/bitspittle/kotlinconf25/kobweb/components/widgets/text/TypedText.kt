98265b79793b6d46d7c3792647b1206ff4966da1
David Herman <bitspittle@gmail.com>
2025-05-07 17:25:42 -0700

    Add TypedText composable

---
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/widgets/text/TypedText.kt
@@ -0,0 +1,29 @@
+package dev.bitspittle.kotlinconf25.kobweb.components.widgets.text
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.browser.util.setInterval
+import com.varabyte.kobweb.compose.dom.ref
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.silk.components.text.SpanText
+import dev.bitspittle.kotlinconf25.kobweb.style.AnimSpeeds
+import kotlinx.browser.window
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+// Initial delay default chosen so that text doesn't start animating in while slides / sections are transitioning.
+@Composable
+fun TypedText(
+    value: String,
+    modifier: Modifier = Modifier,
+    keystrokeDelay: Duration = 50.milliseconds,
+    initialDelay: Duration = AnimSpeeds.Quick
+) {
+    var currText by remember { mutableStateOf("") }
+    SpanText(currText, modifier, ref = ref {
+        window.setInterval(initialDelay, keystrokeDelay) {
+            if (currText.length < value.length) {
+                currText += value[currText.length]
+            } else cancelInterval()
+        }
+    })
+}
