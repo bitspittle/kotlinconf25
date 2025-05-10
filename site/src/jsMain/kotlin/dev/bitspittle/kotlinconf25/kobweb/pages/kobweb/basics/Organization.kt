fa28382d224b20f2db579c68517ecbdbcf6cc7a7
David Herman <bitspittle@gmail.com>
2025-05-10 09:20:04 -0700

    Finish organization slide

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/kobweb/basics/Organization.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/kobweb/basics/Organization.kt
@@ -5,24 +5,37 @@ package dev.bitspittle.kotlinconf25.kobweb.pages.kobweb.basics
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.varabyte.kobweb.compose.ui.modifiers.height
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.core.layout.Layout
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
 import dev.bitspittle.kotlinconf25.kobweb.components.layouts.SlideSection
 import dev.bitspittle.kotlinconf25.kobweb.components.layouts.SlideTitle
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.code.CodeBlock
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.list.Bullets
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.list.Folders
+import dev.bitspittle.kotlinconf25.kobweb.components.widgets.media.Image
 import dev.bitspittle.kotlinconf25.kobweb.util.slides.StepTypes
 import dev.bitspittle.kotlinconf25.kobweb.util.slides.step
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.H3
+import org.jetbrains.compose.web.dom.Span
 
 @InitRoute
 fun initOrganizationPage(ctx: InitRouteContext) {
@@ -124,11 +137,12 @@ fun OrganizationPage() {
                 }
             }
         }
+    }
 
-        SlideSection {
-            CodeBlock(
-                //language=kotlin
-                """
+    SlideSection {
+        CodeBlock(
+            //language=kotlin
+            """
                 import com.mysite.components.sections.NavHeader
                 import com.mysite.components.sections.Footer
 
@@ -142,14 +156,14 @@ fun OrganizationPage() {
                     }
                 }
             """.trimIndent(),
-                highlightLines = "0|4|6,9|1,2,8,10"
-            )
-        }
+            highlightLines = "0|4|6,9|1,2,8,10"
+        )
+    }
 
-        SlideSection {
-            CodeBlock(
-                //language=kotlin
-                """
+    SlideSection {
+        CodeBlock(
+            //language=kotlin
+            """
                 @Page
                 @Composable
                 @Layout(".components.layouts.PageLayout")
@@ -157,10 +171,58 @@ fun OrganizationPage() {
                     /*...*/
                 }
             """.trimIndent(),
-                highlightLines = "0|1|3|5"
-            )
+            highlightLines = "0|1|3|5"
+        )
+    }
+
+    SlideSection {
+        TopStartH3 {
+            Folders {
+                Bullets {
+                    Item("resources/public") {
+                        Item("images") {
+                            Item("buster.jpg")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    SlideSection {
+        SimpleGrid(numColumns(2), Modifier.gap(2.cssRem)) {
+            Column(Modifier.gap(2.cssRem)) {
+                Folders {
+                    Bullets {
+                        Item("resources/public") {
+                            Item("images") {
+                                Item("buster.jpg")
+                            }
+                        }
+                    }
+                }
+
+                Span(Modifier.step(StepTypes.FadeUp).toAttrs()) {
+                    CodeBlock(
+                        //language=kotlin
+                        """
+                            Img(
+                                src = "/images/buster.jpg",
+                                attrs =
+                                    Modifier.fillMaxSize()
+                                        .toAttrs()
+                            )
+                        """.trimIndent(),
+                        highlightLines = "1,2",
+                    )
+                }
+            }
+
+            Image("/assets/images/buster.jpg", Modifier.borderRadius(10.px).step(StepTypes.FadeLeft), scale = 0.81f)
         }
     }
 }
+
+
 
 
