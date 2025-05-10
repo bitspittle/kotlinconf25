ab63260a0e992f26f680e5f7101b5aecff1f471a
David Herman <bitspittle@gmail.com>
2025-05-10 10:35:27 -0700

    Remove one-off color text compsables
    Just prefer using the CSSColorValue extension method instead

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/widgets/text/ColoredText.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/components/widgets/text/ColoredText.kt
@@ -4,23 +4,7 @@ import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.silk.components.text.SpanText
-import dev.bitspittle.kotlinconf25.kobweb.style.SiteColors
 import org.jetbrains.compose.web.css.CSSColorValue
-
-@Composable
-fun KotlinText(value: String) {
-    SiteColors.KotlinPurple.Text(value)
-}
-
-@Composable
-fun KobwebText(value: String) {
-    SiteColors.KobwebBlue.Text(value)
-}
-
-@Composable
-fun HtmlText(value: String) {
-    SiteColors.HtmlOrange.Text(value)
-}
 
 @Composable
 fun CSSColorValue.Text(value: String) {
--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/Index.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/Index.kt
@@ -12,8 +12,8 @@ import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.layout.Layout
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.kotlinconf.KotlinConfLogo
-import dev.bitspittle.kotlinconf25.kobweb.components.widgets.text.KobwebText
-import dev.bitspittle.kotlinconf25.kobweb.components.widgets.text.KotlinText
+import dev.bitspittle.kotlinconf25.kobweb.components.widgets.text.Text
+import dev.bitspittle.kotlinconf25.kobweb.style.SiteColors
 import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.css.em
 import org.jetbrains.compose.web.css.percent
@@ -50,11 +50,11 @@ fun TitlePage() {
                 Text("Build Websites")
                 Br()
                 Text("in ")
-                KotlinText("Kotlin")
+                SiteColors.KotlinPurple.Text("Kotlin")
                 Text(" and ")
-                KotlinText("Compose HTML")
+                SiteColors.KotlinPurple.Text("Compose HTML")
                 Text(" with ")
-                KobwebText("Kobweb")
+                SiteColors.KobwebBlue.Text("Kobweb")
             }
         }
 
