2b972096c033b1f126e491835cbfcb2543be736b
David Herman <bitspittle@gmail.com>
2025-05-10 09:19:57 -0700

    Add directional fades to step types

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/util/slides/Step.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/util/slides/Step.kt
@@ -12,6 +12,7 @@ import dev.bitspittle.kotlinconf25.kobweb.components.widgets.code.substepCount
 import dev.bitspittle.kotlinconf25.kobweb.style.AnimSpeeds
 import dev.bitspittle.kotlinconf25.kobweb.util.toCssUnit
 import org.jetbrains.compose.web.css.DisplayStyle
+import org.jetbrains.compose.web.css.px
 import org.w3c.dom.HTMLElement
 import org.w3c.dom.asList
 import kotlin.time.Duration
@@ -23,29 +24,44 @@ import kotlin.time.Duration
 // See also: SlideLayout, which is responsible for managing steps
 
 object StepTypes {
+    /** Slide down while fading in */
+    const val FadeDown = "fade-down"
+    /** Fade in when the step becomes active */
     const val FadeIn = "fade-in"
+    /** Fade in when the step becomes active and then fade to 50% on the next step */
     const val FadeInThenSemiOut = "fade-in-then-semi-out"
+    /** Slide left while fading in */
+    const val FadeLeft = "fade-left"
+    /** Slide right while fading in */
+    const val FadeRight = "fade-right"
+    /** Slide up while fading in */
+    const val FadeUp = "fade-up"
+    /** Show one step at a time. When not current, this step will not be laid out at all (display: none) */
     const val OneAtATime = "one-at-a-time"
+
+    const val Default = FadeIn
 }
 
-fun Modifier.step(stepType: String = StepTypes.FadeIn, auto: Boolean = false, index: Int? = null) =
+fun Modifier.step(stepType: String = StepTypes.Default, auto: Boolean = false, index: Int? = null) =
     classNames(buildList {
         add("step")
         add(stepType)
         if (auto) add("auto")
     }).thenIfNotNull(index) { Modifier.attr("data-step-index", it.toString())}
 
-fun Modifier.step(stepType: String = StepTypes.FadeIn, delay: Duration, index: Int? = null) =
+fun Modifier.step(stepType: String = StepTypes.Default, delay: Duration, index: Int? = null) =
     attr("data-step-delay", "${delay.inWholeMilliseconds}")
         .step(stepType, auto = true, index = index)
 
 
 @InitSilk
 fun initStepStyles(ctx: InitSilkContext) {
+    val defaultStepSpeed = AnimSpeeds.Fast.toCssUnit()
+
     ctx.stylesheet.apply {
         registerStyleBase(".step.${StepTypes.FadeIn}") {
             Modifier
-                .transition(Transition.of("opacity", AnimSpeeds.Fast.toCssUnit()))
+                .transition(Transition.of("opacity", defaultStepSpeed))
                 .opacity(0)
         }
 
@@ -54,9 +70,61 @@ fun initStepStyles(ctx: InitSilkContext) {
                 .opacity(1)
         }
 
+        registerStyleBase(".step.${StepTypes.FadeUp}") {
+            Modifier
+                .transition(Transition.group(listOf("opacity", "translate"), defaultStepSpeed))
+                .opacity(0)
+                .translateY(40.px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeUp}.active") {
+            Modifier
+                .opacity(1)
+                .translateY(0.px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeDown}") {
+            Modifier
+                .transition(Transition.group(listOf("opacity", "translate"), defaultStepSpeed))
+                .opacity(0)
+                .translateY((-40).px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeDown}.active") {
+            Modifier
+                .opacity(1)
+                .translateY(0.px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeLeft}") {
+            Modifier
+                .transition(Transition.group(listOf("opacity", "translate"), defaultStepSpeed))
+                .opacity(0)
+                .translateX(40.px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeLeft}.active") {
+            Modifier
+                .opacity(1)
+                .translateX(0.px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeRight}") {
+            Modifier
+                .transition(Transition.group(listOf("opacity", "translate"), defaultStepSpeed))
+                .opacity(0)
+                .translateX((-40).px)
+        }
+
+        registerStyleBase(".step.${StepTypes.FadeRight}.active") {
+            Modifier
+                .opacity(1)
+                .translateX(0.px)
+        }
+
         registerStyleBase(".step.${StepTypes.FadeInThenSemiOut}") {
             Modifier
-                .transition(Transition.of("opacity", AnimSpeeds.Quick.toCssUnit()))
+                .transition(Transition.of("opacity", defaultStepSpeed))
         }
 
         registerStyleBase(".step.${StepTypes.FadeInThenSemiOut}:not(.active)") {
