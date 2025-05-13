3a53d5e3e1093e6e79f767b4c09ff68932c5b90c
David Herman <bitspittle@gmail.com>
2025-05-12 22:37:03 -0700

    Finish API routes slide

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/kobweb/fullstack/ApiRoutes.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/kobweb/fullstack/ApiRoutes.kt
@@ -5,6 +5,7 @@ package dev.bitspittle.kotlinconf25.kobweb.pages.kobweb.fullstack
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.MinWidth
+import com.varabyte.kobweb.compose.css.Transition
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -15,27 +16,56 @@ import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
 import com.varabyte.kobweb.compose.ui.modifiers.gap
 import com.varabyte.kobweb.compose.ui.modifiers.minWidth
+import com.varabyte.kobweb.compose.ui.modifiers.opacity
+import com.varabyte.kobweb.compose.ui.modifiers.size
+import com.varabyte.kobweb.compose.ui.modifiers.transition
+import com.varabyte.kobweb.compose.ui.modifiers.translateY
+import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.core.layout.Layout
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.init.registerStyleBase
 import dev.bitspittle.kotlinconf25.kobweb.components.layouts.MultiPartSlideLayoutScope
 import dev.bitspittle.kotlinconf25.kobweb.components.layouts.SlideTitle
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.code.CodeBlock
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.list.Bullets
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.list.Folders
 import dev.bitspittle.kotlinconf25.kobweb.style.Gaps
+import dev.bitspittle.kotlinconf25.kobweb.util.slides.DefaultStepSpeed
 import dev.bitspittle.kotlinconf25.kobweb.util.slides.StepTypes
+import dev.bitspittle.kotlinconf25.kobweb.util.slides.StepTypes.OneAtATime
 import dev.bitspittle.kotlinconf25.kobweb.util.slides.step
 import org.jetbrains.compose.web.css.cssRem
 import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.H3
 
 @InitRoute
 fun initApiRoutesPage(ctx: InitRouteContext) {
     ctx.data.add(SlideTitle("API routes"))
+}
+
+private const val ShiftExampleDown = "api-route-shift-down"
+
+@InitSilk
+fun initShiftStepEffect(ctx: InitSilkContext) {
+    ctx.stylesheet.apply {
+        registerStyleBase(".step.$ShiftExampleDown") {
+            Modifier
+                .translateY((-60).percent)
+                .transition(Transition.of("translate", DefaultStepSpeed))
+        }
+
+        registerStyleBase(".step.$ShiftExampleDown.active") {
+            Modifier.translateY(60.percent)
+        }
+    }
 }
 
 @Page
@@ -102,6 +132,121 @@ fun MultiPartSlideLayoutScope.ApiRoutesPage() {
                     preModifier = Modifier.fillMaxWidth()
                 )
             }
+        }
+    }
+
+    // Request type, return code, request body, and data
+    SlideSection {
+        Box(Modifier.step(StepTypes.OneAtATime, auto = true)) {
+            CodeBlock(
+                """
+                @Api
+                suspend fun postMessage(ctx: ApiContext) {
+                    if (ctx.req.method != HttpMethod.POST) return
+    
+                    // Get message from request
+                    // Store message in a datastore
+    
+                    ctx.res.status = 200
+                }
+
+
+                """.trimIndent(),
+                preModifier = Modifier.fillMaxWidth(),
+                highlightLines = "0|3|8|5"
+            )
+        }
+        Box(Modifier.step(StepTypes.OneAtATime)) {
+            CodeBlock(
+                """
+                @Api
+                suspend fun postMessage(ctx: ApiContext) {
+                    if (ctx.req.method != HttpMethod.POST) return
+    
+                    val message = ctx.req.readBody()
+                    // Store message in a datastore
+    
+                    ctx.res.status = 200
+                }
+
+
+                """.trimIndent(),
+                preModifier = Modifier.fillMaxWidth(),
+                highlightLines = "5|6"
+            )
+        }
+        Box(Modifier.step(StepTypes.OneAtATime)) {
+            CodeBlock(
+                """
+                @Api
+                suspend fun postMessage(ctx: ApiContext) {
+                    if (ctx.req.method != HttpMethod.POST) return
+    
+                    val message = ctx.req.readBody()
+                    ctx.data.getValue<Messages>().add(message)
+    
+                    ctx.res.status = 200
+                }
+
+
+                """.trimIndent(),
+                preModifier = Modifier.fillMaxWidth(),
+                highlightLines = "6|8"
+            )
+        }
+        Box(Modifier.step(StepTypes.OneAtATime)) {
+            CodeBlock(
+                """
+                @Api
+                suspend fun postMessage(ctx: ApiContext) {
+                    if (ctx.req.method != HttpMethod.POST) return
+    
+                    val message = ctx.req.readBody()
+                    ctx.data.getValue<MessageStore>().add(message)
+    
+                    ctx.res.setBodyText("OK")
+                    // Side effect: sets res code to 200
+                }
+                """.trimIndent(),
+                preModifier = Modifier.fillMaxWidth(),
+                highlightLines = "8|0"
+            )
+        }
+    }
+
+    SlideSection {
+        fun Modifier.codeWidth() = this.width(70.percent)
+
+        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Box(Modifier.step(ShiftExampleDown).codeWidth()) {
+                CodeBlock(
+                    """
+                    @Api
+                    suspend fun postMessage(ctx: ApiContext) {
+                        ctx.data.getValue<Messages>()
+                    }
+                    """.trimIndent(),
+                    preModifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+        Box(Modifier.step(StepTypes.FadeDown, auto = true).codeWidth()) {
+            CodeBlock(
+                """
+                    @InitApi
+                    fun initMessages(ctx: InitApiContext) {
+                        ctx.data.add(Messages())
+                    }
+                    """.trimIndent(),
+                preModifier = Modifier.fillMaxWidth().translateY((-60).percent),
+                highlightLines = "0|1|2|3"
+            )
+        }
+    }
+
+    SlideSection {
+        H3 {
+            Link("/demo/guestbook")
         }
     }
 }
