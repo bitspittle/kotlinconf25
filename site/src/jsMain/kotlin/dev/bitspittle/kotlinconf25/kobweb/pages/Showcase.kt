dc2a2ba54ee77f9e4319618ae6e5ec77fd67e27e
David Herman <bitspittle@gmail.com>
2025-05-13 21:19:30 -0700

    Add URLs to showcase slides

--- site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/Showcase.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/Showcase.kt
@@ -1,18 +1,25 @@
 package dev.bitspittle.kotlinconf25.kobweb.pages
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.border
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.gap
+import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.core.data.add
 import com.varabyte.kobweb.core.init.InitRoute
 import com.varabyte.kobweb.core.init.InitRouteContext
 import com.varabyte.kobweb.core.layout.Layout
+import com.varabyte.kobweb.silk.components.navigation.Link
 import dev.bitspittle.kotlinconf25.kobweb.components.layouts.MultiPartSlideLayoutScope
 import dev.bitspittle.kotlinconf25.kobweb.components.layouts.SlideTitle
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.media.Image
 import dev.bitspittle.kotlinconf25.kobweb.components.widgets.media.Video
+import dev.bitspittle.kotlinconf25.kobweb.style.Gaps
 import dev.bitspittle.kotlinconf25.kobweb.style.SiteColors
 import dev.bitspittle.kotlinconf25.kobweb.util.slides.StepTypes
 import dev.bitspittle.kotlinconf25.kobweb.util.slides.step
@@ -28,28 +35,53 @@ fun initCodeExamplePage(ctx: InitRouteContext) {
 @Composable
 @Layout(".components.layouts.MultiPartSlideLayout")
 fun MultiPartSlideLayoutScope.ShowcasePage() {
+    @Composable
+    fun UrlAndContent(url: String, content: @Composable () -> Unit) {
+        Column(Modifier.fillMaxSize().gap(Gaps.Normal).padding(top = Gaps.Normal), horizontalAlignment = Alignment.CenterHorizontally) {
+            Link(url)
+            content()
+        }
+    }
+
     SlideSection {
-        Video("/assets/showcase/yt-reimagined.mp4", Modifier.step(StepTypes.OneAtATime, auto = true), loop = true, scale = 0.6f)
-        Video("/assets/showcase/yt-reimagined2.mp4", Modifier.step(StepTypes.OneAtATime), loop = true, scale = 0.6f)
-        Image("/assets/showcase/yt-reimagined3.png", Modifier.step(StepTypes.OneAtATime), scale = 0.8f)
+        UrlAndContent("https://github.com/shubhamsinghshubham777/YouTubeRedesigned") {
+            Video(
+                "/assets/showcase/yt-reimagined.mp4",
+                Modifier.step(StepTypes.OneAtATime, auto = true),
+                loop = true,
+                scale = 0.6f
+            )
+            Video("/assets/showcase/yt-reimagined2.mp4", Modifier.step(StepTypes.OneAtATime), loop = true, scale = 0.6f)
+            Image("/assets/showcase/yt-reimagined3.png", Modifier.step(StepTypes.OneAtATime), scale = 0.6f)
+        }
     }
     SlideSection {
-        Video("/assets/showcase/kotfolio.mp4", scale = 1.1f)
+        UrlAndContent("https://kotfolio.onrender.com/") {
+            Video("/assets/showcase/kotfolio.mp4", scale = 1.1f)
+        }
     }
     SlideSection {
-        Video("/assets/showcase/kore.mp4", scale = 1.2f)
+        UrlAndContent("https://kore.ayfri.com/") {
+            Video("/assets/showcase/kore.mp4", scale = 1.2f)
+        }
     }
     SlideSection {
-        Video("/assets/showcase/chess-playground.mp4", scale = 1.3f)
+        UrlAndContent("https://chess-playground.fly.dev/") {
+            Video("/assets/showcase/chess-playground.mp4", scale = 1.3f)
+        }
     }
     SlideSection {
-        Video("/assets/showcase/phoenix-red-wolf.mp4")
+        UrlAndContent("https://bitspittle.github.io/droidcon-sf-24/") {
+            Video("/assets/showcase/phoenix-red-wolf.mp4")
+            }
     }
     SlideSection {
         Video("/assets/showcase/rodrigo-wedding.mp4", scale = 1.1f)
     }
     SlideSection {
-        Video("/assets/showcase/fluense.mp4", scale = 1.1f)
+        UrlAndContent("https://fluen.se/en/") {
+            Video("/assets/showcase/fluense.mp4", scale = 1.1f)
+        }
     }
     SlideSection {
         Image(
