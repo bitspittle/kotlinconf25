1a543c0fb9b24ceb0e5d09b80e57d5dfac09a132
David Herman <bitspittle@gmail.com>
2025-05-21 17:16:29 +0200

    I thought I fixed the reset script already??

--- reset-talk.sh
+++ reset-talk.sh
@@ -1,5 +1,5 @@
 git reset --hard
+git clean -df
 rmdir site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/demo 2>/dev/null
-rmdir site/src/commonMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/model 2>/dev/null
 rmdir site/src/jvmMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/api/guestbook 2>/dev/null
 
