d3a10c46eee3b652992b19e6ca3c5d2b1cdc083c
David Herman <bitspittle@gmail.com>
2025-05-21 05:48:33 +0200

    Add dummy bookmark files

--- site/src/commonMain/kotlin/dev/bitspittle/kotilnconf25/kobweb/Root.kt
+++
@@ -1,5 +0,0 @@
-package dev.bitspittle.kotilnconf25.kobweb
-
-// This is a fake file which exists to keep the kobweb commonMain folder alive, so it will still be around between
-// talks after I git nuke the project to reset the code back to a clean slate.
-
