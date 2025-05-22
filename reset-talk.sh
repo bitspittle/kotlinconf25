f46a9db2efb9dc5777aeffe2fb9e69ac0e00ceeb
David Herman <bitspittle@gmail.com>
2025-05-22 05:40:49 +0200

    Nit: Update reset script

--- reset-talk.sh
+++ reset-talk.sh
@@ -1,4 +1,5 @@
-git reset --hard
+git checkout -f main
+git reset --hard origin/main
 git clean -df
 rmdir site/src/jsMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/pages/demo 2>/dev/null
 rmdir site/src/jvmMain/kotlin/dev/bitspittle/kotlinconf25/kobweb/api/guestbook 2>/dev/null
