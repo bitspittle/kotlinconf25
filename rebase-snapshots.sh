0d4fc9959380febb11bedc15e88e9b423de4d9f4
David Herman <bitspittle@gmail.com>
2025-05-22 16:15:18 +0200

    Make a backup of snapshot branches

--- rebase-snapshots.sh
+++ rebase-snapshots.sh
@@ -1,13 +1,17 @@
 git checkout snapshot/kcFe1
 git rebase main
+git push --force
 
 git checkout snapshot/kcFe2
 git rebase snapshot/kcFe1
+git push --force
 
 git checkout snapshot/kcFe3
 git rebase snapshot/kcFe2
+git push --force
 
 git checkout snapshot/kcBe
 git rebase snapshot/kcFe3
+git push --force
 
 git checkout main
